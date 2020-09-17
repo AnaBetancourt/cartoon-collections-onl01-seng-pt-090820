756d155f53aa5d37bada9d2ddd1c4af25321fade
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-09-17 16:24:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,8 @@
-{ roll_call_dwarves(names)
-  names.each_with_index {|name, index| 
-    puts "#{index + 1} #{name}"}
-}
+def roll_call_dwarves(names)
+  names.each_with_index do |name, index| 
+    puts "#{index + 1} #{name}"
+  end
+end
 
 def summon_captain_planet(elements)
   elements.map do |call|
